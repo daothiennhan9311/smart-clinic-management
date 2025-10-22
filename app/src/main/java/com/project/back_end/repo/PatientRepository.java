261deb56a5c61ed8f6591f449a63e7c4f12144e7
daothiennhan9311 <daothiennhan9311@gmail.com>
2025-10-23 00:09:40 +0700

    Create PatientRepository.java

---
+++ app/src/main/java/com/project/back_end/repo/PatientRepository.java
@@ -0,0 +1,11 @@
+package com.project.back_end.repo;
+
+import com.project.back_end.models.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Integer> {
+    // You can add custom queries here if needed, for example:
+    // List<Patient> findByNameContaining(String name);
+}
